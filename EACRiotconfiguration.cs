ae93bfed92b13ebe2f2d8baa0c5ce2a89ff78fda
Ekin <76937703+adaekin@users.noreply.github.com>
2024-12-08 16:34:07 +0300

    Add files via upload

---
+++ EACRiotconfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotGadgets
+{
+    public class EACRiotconfiguration : IRocketPluginConfiguration
+    {
+        public List<ushort> TaserWeapons = new List<ushort>();
+        public bool Tear_Gas;
+        public int Neutralized_Time;
+        public void LoadDefaults()
+        {
+            TaserWeapons = new List<ushort> { 1023, 1337 };
+            Neutralized_Time = 3;
+            Tear_Gas = false;
+
+        }
+    }
+}
